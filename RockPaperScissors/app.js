8c70fe248a285c7d7af570d1ad6c7d77787a0a66
Antonio Kim <antoniok@sfu.ca>
2022-02-24 23:11:11 -0800

    add playRound function to simulate result

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -4,4 +4,42 @@ const computerPlay = () => {
   return computerOptions[computerChoose];
 }
 
-console.log(computerPlay());
+const playRound = (playerSelection, computerSelection) => {
+  let result = "";
+  switch (playerSelection) {
+    case "Rock":
+      if (computerSelection === "Paper") 
+        result = "You lose! Paper beats Rock";      
+      else if (computerSelection === "Scissor")
+        result = "You win! Rock beats Scissor";
+      else
+        result = "It's a tie!";
+      break;
+    
+    case "Paper":
+      if (computerSelection === "Rock")
+        result = "You win! Paper wins Rock";
+      else if (computerSelection === "Scissor")
+        result = "You lose! Scissor wins Paper";
+      else
+        result = "It's a tie!";
+      break;
+    
+    case "Scissor":
+      if (computerSelection === "Paper")
+        result = "You win! Scissor wins Paper";
+      else if (computerSelection === "Rock")
+        result = "You lose! Rock wins Scissor";
+      else
+        result = "It's a tie!";
+      break;
+
+    default:
+      result = "Unknown Input. Please try again.";
+  }
+  return result
+}
+
+const playerSelection = "Scissor";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
