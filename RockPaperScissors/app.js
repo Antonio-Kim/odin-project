f0df970c49026dba2b15ed0d0b7ba89aeccb69da
Antonio Kim <antoniok@sfu.ca>
2022-02-25 00:49:47 -0800

    completed Rock, Paper, Scissor Project

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -8,44 +8,68 @@ const playRound = (playerSelection, computerSelection) => {
   let result = "";
   switch (playerSelection) {
     case "Rock":
-      if (computerSelection === "Paper") 
-        result = "You lose! Paper beats Rock";      
-      else if (computerSelection === "Scissor")
-        result = "You win! Rock beats Scissor";
-      else
-        result = "It's a tie!";
+      if (computerSelection === "Paper") {
+        console.log("You lose! Paper beats Rock")
+        result = "computer";  
+      } else if (computerSelection === "Scissor") {
+        console.log("You win! Rock beats Scissor");
+        result = "player";
+      } else {
+        console.log("It's a tie!");
+        result = "tie";
+      }
       break;
     
     case "Paper":
-      if (computerSelection === "Rock")
-        result = "You win! Paper wins Rock";
-      else if (computerSelection === "Scissor")
-        result = "You lose! Scissor wins Paper";
-      else
-        result = "It's a tie!";
+      if (computerSelection === "Rock") {
+        console.log("You win! Paper wins Rock");
+        result = "player";
+      } else if (computerSelection === "Scissor") {
+        console.log("You lose! Scissor wins Paper");
+        result = "computer";
+      } else {
+        console.log("It's a tie!");
+        result = "tie";
+      }
       break;
     
     case "Scissor":
-      if (computerSelection === "Paper")
-        result = "You win! Scissor wins Paper";
-      else if (computerSelection === "Rock")
-        result = "You lose! Rock wins Scissor";
-      else
-        result = "It's a tie!";
+      if (computerSelection === "Paper") {
+        console.log("You win! Scissor wins Paper");
+        result = "player";
+      } else if (computerSelection === "Rock") {
+        console.log("You lose! Rock wins Scissor");
+        result = "computer";
+      } else {
+        console.log("It's a tie!");
+        result = "tie";
+      }
       break;
 
     default:
-      result = "Unknown Input. Please try again.";
+      console.log("Unknown Input. Please try again.");
   }
   return result
 }
 
 const game = () => {
+  let computerRecord = 0;
+  let playerRecord = 0;
   for (let i = 0; i < 5; i++) {
     const playerSelection = prompt("Enter Rock, Paper, or Scissor: ");
     const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+    let result = playRound(playerSelection, computerSelection);
+    if (result === "player")
+      playerRecord++;
+    else if (result === "computer")
+      computerRecord++;
   }
+  if (playerRecord > computerRecord) 
+    console.log("You win! congratulation!");
+  else if (playerRecord < computerRecord)
+    console.log("You lost. Try again.");
+  else 
+    console.log("You both tied!");
 }
 
 game();
